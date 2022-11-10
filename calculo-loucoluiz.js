7508c19e3efc6ebef0e9dfe8acb7dc4a6ea219df
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2022-11-10 12:25:10 -0300

    Add files via upload

---
+++ calculo-loucoluiz.js
@@ -0,0 +1,17 @@
+let ac = 3
+let agh = 4.5
+let tri1 = 6
+let tri2 = 5
+
+
+let at = ((ac*3+agh*2)-60)/5
+console.log("Nota da at para tirar 6 no trimestre: "+at)
+
+let tri3 = ((tri1 + tri2*2) -36)/3
+console.log("Nota do trimestre para passar de ano: "+tri3)
+
+let ano = ((ac*3+agh*2)+tri3*10)/5
+console.log("Nota da at para passar de ano: "+ano)
+
+
+// 5,58
